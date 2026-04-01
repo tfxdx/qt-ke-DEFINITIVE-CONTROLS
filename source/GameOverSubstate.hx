b3107da69019adff85a3269aba4dcc52881cce5d
tfxdx <yunumendozaperez@gmail.com>
2026-04-01 16:03:23 -0500

    Update GameOverSubstate.hx6

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -66,6 +66,10 @@ class GameOverSubstate extends MusicBeatSubstate
 		FlxG.camera.target = null;
 
 		bf.playAnim('firstDeath');
+
+		#if android
+		addVirtualPad(NONE, A_B);
+		#end
 	}
 
 	override function update(elapsed:Float)
