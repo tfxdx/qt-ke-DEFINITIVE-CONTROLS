280e98cccf8ce3241653e27b392636f84640da81
tfxdx <yunumendozaperez@gmail.com>
2026-04-02 18:54:14 -0500

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -303,7 +303,7 @@ class Controls extends FlxActionSet
 				inline forEachBound(Control.UP, (action, state) -> addbutton(action, virtualPad.buttonUp, state));
 				inline forEachBound(Control.LEFT, (action, state) -> addbutton(action, virtualPad.buttonLeft, state));
 				inline forEachBound(Control.RIGHT, (action, state) -> addbutton(action, virtualPad.buttonRight, state));
-			case FULL | RIGHT_FULL:
+			case FULL:
 				inline forEachBound(Control.UP, (action, state) -> addbutton(action, virtualPad.buttonUp, state));
 				inline forEachBound(Control.DOWN, (action, state) -> addbutton(action, virtualPad.buttonDown, state));
 				inline forEachBound(Control.LEFT, (action, state) -> addbutton(action, virtualPad.buttonLeft, state));
