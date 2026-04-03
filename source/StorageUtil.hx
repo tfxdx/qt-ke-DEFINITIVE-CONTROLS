31372ee4aae56f93de4e8d2907bd76c3735eb1a5
tfxdx <yunumendozaperez@gmail.com>
2026-04-03 11:47:28 -0500

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -76,7 +76,6 @@ class StorageUtil
 			AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
 		if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
 			 && !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-			CoolUtil.showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens','Notice!');
 		try
 		{
 			if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
