a6e3f9334bf243618420a3be40dcfe8d7779e756
tfxdx <yunumendozaperez@gmail.com>
2026-04-03 17:01:02 -0500

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -12,59 +12,50 @@ import android.Tools;
 import android.callback.CallBack;
 #end
 using StringTools;
-
-class StorageUtil
-{
+class StorageUtil {
 	#if sys
-	public static function getStorageDirectory():String
-		return #if android haxe.io.Path.addTrailingSlash(AndroidContext.getExternalFilesDir()) #elseif ios lime.system.System.documentsDirectory #else Sys.getCwd() #end;
-
-	public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void
-	{
-		try
-		{
-			if (!FileSystem.exists('saves'))
-				FileSystem.createDirectory('saves');
-
-			File.saveContent('saves/$fileName', fileData);
-			if (alert)
-				CoolUtil.showPopUp('$fileName has been saved.', "Success!");
-		}
-		catch (e:Dynamic)
-			if (alert)
-				CoolUtil.showPopUp('$fileName couldn\'t be saved.\n(${e.message})', "Error!")
-			else
-				trace('$fileName couldn\'t be saved. (${e.message})');
-	}
-
+	public static function getStorageDirectory():String return
+	#if android
+	haxe.io.Path.addTrailingSlash(AndroidContext.getExternalFilesDir())
+	#elseif ios
+	lime.system.System.documentsDirectory #else Sys.getCwd()
+	#end;
+	public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void {
+	try {
+	if (!FileSystem.exists('saves'))
+	FileSystem.createDirectory('saves');
+	File.saveContent('saves/$fileName', fileData);
+	if (alert)
+	CoolUtil.showPopUp('$fileName has been saved.', "Success!"); }
+	catch (e:Dynamic)
+	if (alert)
+	CoolUtil.showPopUp('$fileName couldn\'t be saved.\n(${e.message})', "Error!")
+	else
+	trace('$fileName couldn\'t be saved. (${e.message})'); }
 	#if android
 	public static function requestPermissions():Void
 	{
-		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
-			AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
-		else
-			AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
-
-		if (!AndroidEnvironment.isExternalStorageManager())
-			AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
-
-		if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
-			&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
-			|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
-				&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-			CoolUtil.showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens','Notice!');
-
-		try
-		{
-			if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
-				FileSystem.createDirectory(StorageUtil.getStorageDirectory());
-		}
-		catch (e:Dynamic)
-		{
-			CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game', 'Error!');
-			lime.system.System.exit(1);
-		}
+	if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
+	AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
+	else
+	AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
+	if (!AndroidEnvironment.isExternalStorageManager())
+	AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+	if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
+	&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
+	|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
+	&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
+	CoolUtil.showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens','Notice!');
+	try
+	{
+	if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
+	FileSystem.createDirectory(StorageUtil.getStorageDirectory());
+	}
+	catch (e:Dynamic)
+	{
+	CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game', 'Error!');
+	lime.system.System.exit(1);
+	}
 	}
 	#end
-	#end
-}
+	#end }
