18edec8cfc0bb61ce244f1ab2a4e8b853f5ae04c
tfxdx <yunumendozaperez@gmail.com>
2026-04-03 17:02:54 -0500

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -3,59 +3,116 @@ package;
 import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
+
 #if sys
 import sys.*;
 import sys.io.*;
 #end
+
 #if android
 import android.Tools;
 import android.callback.CallBack;
 #end
+
 using StringTools;
-class StorageUtil {
-	#if sys
-	public static function getStorageDirectory():String return
-	#if android
-	haxe.io.Path.addTrailingSlash(AndroidContext.getExternalFilesDir())
-	#elseif ios
-	lime.system.System.documentsDirectory #else Sys.getCwd()
-	#end;
-	public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void {
-	try {
-	if (!FileSystem.exists('saves'))
-	FileSystem.createDirectory('saves');
-	File.saveContent('saves/$fileName', fileData);
-	if (alert)
-	CoolUtil.showPopUp('$fileName has been saved.', "Success!"); }
-	catch (e:Dynamic)
-	if (alert)
-	CoolUtil.showPopUp('$fileName couldn\'t be saved.\n(${e.message})', "Error!")
-	else
-	trace('$fileName couldn\'t be saved. (${e.message})'); }
-	#if android
-	public static function requestPermissions():Void
-	{
-	if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
-	AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
-	else
-	AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
-	if (!AndroidEnvironment.isExternalStorageManager())
-	AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
-	if ((AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU
-	&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
-	|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
-	&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-	CoolUtil.showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens','Notice!');
-	try
-	{
-	if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
-	FileSystem.createDirectory(StorageUtil.getStorageDirectory());
-	}
-	catch (e:Dynamic)
-	{
-	CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game', 'Error!');
-	lime.system.System.exit(1);
-	}
-	}
-	#end
-	#end }
+
+class StorageUtil 
+{
+    #if sys
+    public static function getStorageDirectory():String 
+    {
+        return
+        #if android
+            Path.addTrailingSlash(AndroidContext.getExternalFilesDir())
+        #elseif ios
+            LimeSystem.documentsDirectory
+        #else
+            Sys.getCwd()
+        #end;
+    }
+
+    public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void 
+    {
+        try 
+        {
+            if (!FileSystem.exists('saves'))
+                FileSystem.createDirectory('saves');
+
+            File.saveContent('saves/$fileName', fileData);
+
+            if (alert)
+                CoolUtil.showPopUp('$fileName has been saved.', "Success!");
+        } 
+        catch (e:Dynamic) 
+        {
+            if (alert)
+                CoolUtil.showPopUp('$fileName couldn\'t be saved.\n(${e.message})', "Error!");
+            else
+                trace('$fileName couldn\'t be saved. (${e.message})');
+        }
+    }
+
+    #if android
+    public static function requestPermissions():Void
+    {
+        if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
+        {
+            AndroidPermissions.requestPermissions([
+                'READ_MEDIA_IMAGES',
+                'READ_MEDIA_VIDEO',
+                'READ_MEDIA_AUDIO',
+                'READ_MEDIA_VISUAL_USER_SELECTED'
+            ]);
+        }
+        else
+        {
+            AndroidPermissions.requestPermissions([
+                'READ_EXTERNAL_STORAGE',
+                'WRITE_EXTERNAL_STORAGE'
+            ]);
+        }
+
+        if (!AndroidEnvironment.isExternalStorageManager())
+        {
+            AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+        }
+
+        if (
+            (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU &&
+             !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
+            ||
+            (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU &&
+             !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE'))
+        )
+        {
+            CoolUtil.showPopUp(
+                'If you accepted the permissions you are all good!\n' +
+                'If you didn\'t then expect a crash\n' +
+                'Press OK to see what happens',
+                'Notice!'
+            );
+        }
+
+        try
+        {
+            if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
+            {
+                FileSystem.createDirectory(StorageUtil.getStorageDirectory());
+            }
+        }
+        catch (e:Dynamic)
+        {
+            CoolUtil.showPopUp(
+                'Please create directory to\n' +
+                StorageUtil.getStorageDirectory() +
+                '\nPress OK to close the game',
+                'Error!'
+            );
+
+            LimeSystem.exit(1);
+        }
+    }
+    #end
+
+    #end
+}
