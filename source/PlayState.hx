449993b9021c58f2c86fb9d66da669b6b983a792
tfxdx <yunumendozaperez@gmail.com>
2026-04-05 03:22:48 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4637,10 +4637,10 @@ class PlayState extends MusicBeatState
 			//Dodge code, yes it's bad but oh well. -Haz
 			//var dodgeButton = controls.ACCEPT; //I have no idea how to add custom controls so fuck it. -Haz
 
-			if(FlxG.keys.justPressed.SPACE || addAndroidControls._vpad.buttonA.justPressed)
+			if(FlxG.keys.justPressed.SPACE || dodgeButton.buttonA.justPressed)
 				trace('butttonpressed');
 
-			if(FlxG.keys.justPressed.SPACE || addAndroidControls._vpad.buttonA.justPressed && !bfDodging && bfCanDodge){
+			if(FlxG.keys.justPressed.SPACE || dodgeButton.buttonA.justPressed && !bfDodging && bfCanDodge){
 				trace('DODGE START!');
 				bfDodging = true;
 				bfCanDodge = false;
