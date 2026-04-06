dcdb0f3e12a50a1dd16be51843c093697dbed4d7
tfxdx <yunumendozaperez@gmail.com>
2026-04-06 16:20:21 -0500

    Update AndroidControls.hx

--- source/mobile/AndroidControls.hx
+++ source/mobile/AndroidControls.hx
@@ -7,72 +7,14 @@ import flixel.math.FlxPoint;
 import mobile.FlxVirtualPad;
 import mobile.FlxHitbox;
 
-class Config {
-	var save:FlxSave;
-
-	public function new() {
-		save = new FlxSave();
-		save.bind("savecontrol");
-	}
-
-	public function getcontrolmode():Int {
-		if (save.data.buttonsmode != null) 
-			return save.data.buttonsmode[0];
-		return 0;
-	}
-
-	public function setcontrolmode(mode:Int = 0):Int {
-		if (save.data.buttonsmode == null) save.data.buttonsmode = new Array();
-		save.data.buttonsmode[0] = mode;
-		save.flush();
-
-		return save.data.buttonsmode[0];
-	}
-
-	public function savecustom(_pad:FlxVirtualPad) {
-		if (save.data.buttons == null) {
-			save.data.buttons = new Array();
-
-			for (buttons in _pad) {
-				save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
-			}
-		}
-		else {
-			var tempCount:Int = 0;
-			for (buttons in _pad) {
-				save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
-				tempCount++;
-			}
-		}
-		save.flush();
-	}
-
-	public function loadcustom(_pad:FlxVirtualPad):FlxVirtualPad {
-		if (save.data.buttons == null) 
-			return _pad;
-		var tempCount:Int = 0;
-
-		for(buttons in _pad) {
-			buttons.x = save.data.buttons[tempCount].x;
-			buttons.y = save.data.buttons[tempCount].y;
-			tempCount++;
-		}	
-		return _pad;
-	}
-}
-
 class AndroidControls extends FlxSpriteGroup {
 	public var mode:ControlsGroup = HITBOX;
 
 	public var hbox:FlxHitbox;
 	public var vpad:FlxVirtualPad;
 
-	var config:Config;
-
 	public function new() {
 		super();
-		
-		config = new Config();
 
 		mode = getModeFromNumber(config.getcontrolmode());
 
