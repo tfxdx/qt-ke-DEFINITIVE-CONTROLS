5a9350ec5395bc8671110f22d34eb7a565820aee
tfxdx <yunumendozaperez@gmail.com>
2026-04-13 02:23:00 -0500

    Update StorageUtil.hx

--- source/StorageUtil.hx
+++ source/StorageUtil.hx
@@ -10,8 +10,15 @@ import sys.io.*;
 #end
 
 #if android
-import android.Tools;
-import android.callback.CallBack;
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
 using StringTools;
