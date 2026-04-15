53cb38a7e5e94a49cc8b37bae9c0890b1bceec08
tfxdx <yunumendozaperez@gmail.com>
2026-04-14 20:54:13 -0500

    Update CopyState.hx

--- source/CopyState.hx
+++ source/CopyState.hx
@@ -20,7 +20,7 @@ using StringTools;
 
 class CopyState extends MusicBeatState
 {
-	private static final textFilesExtensions:Array<String> = ['ini', 'txt', 'xml', 'hxs', 'hx', 'lua', 'json', 'frag', 'vert'];
+	private static final textFilesExtensions:Array<String> = [];
 	public static final IGNORE_FOLDER_FILE_NAME:String = "CopyState-Ignore.txt";
 	private static var directoriesToIgnore:Array<String> = [];
 	public static var locatedFiles:Array<String> = [];
