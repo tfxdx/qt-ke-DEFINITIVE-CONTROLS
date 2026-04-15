8e9f09d2d87f6a8b5e35d3cbeb0c53f07955c2b7
tfxdx <yunumendozaperez@gmail.com>
2026-04-15 17:04:34 -0500

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -281,10 +281,10 @@ class Controls extends FlxActionSet
 	}
 
 	public function setHitBox(Hitbox:FlxHitbox) {
-		inline forEachBound(Control.UP, (action, state) -> addButton(action, Hitbox.buttonUp, state));
-		inline forEachBound(Control.DOWN, (action, state) -> addButton(action, Hitbox.buttonDown, state));
-		inline forEachBound(Control.LEFT, (action, state) -> addButton(action, Hitbox.buttonLeft, state));
-		inline forEachBound(Control.RIGHT, (action, state) -> addButton(action, Hitbox.buttonRight, state));
+		inline forEachBound(Control.UP, (action, state) -> addbutton(action, Hitbox.buttonUp, state));
+		inline forEachBound(Control.DOWN, (action, state) -> addbutton(action, Hitbox.buttonDown, state));
+		inline forEachBound(Control.LEFT, (action, state) -> addbutton(action, Hitbox.buttonLeft, state));
+		inline forEachBound(Control.RIGHT, (action, state) -> addbutton(action, Hitbox.buttonRight, state));
 	}
 	
 	public function setVirtualPad(virtualPad:FlxVirtualPad, ?DPad:FlxDPadMode, ?Action:FlxActionMode) {
