9a7eec22752ebf77e6c4897d1f99a0177deb76e8
tfxdx <yunumendozaperez@gmail.com>
2026-04-15 16:50:10 -0500

    Update FlxHitbox.hx

--- source/mobile/FlxHitbox.hx
+++ source/mobile/FlxHitbox.hx
@@ -4,7 +4,7 @@ import flash.display.BitmapData;
 import flash.display.Shape;
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
-import flixel.ui.FlxButton;
+import mobile.FlxButton;
 
 /**
  * A zone with 4 hint's (A hitbox).
